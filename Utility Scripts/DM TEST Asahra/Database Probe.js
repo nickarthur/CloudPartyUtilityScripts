c81615367f754583daf2d8cb50faaac4ee65cadf
nickarthur <metrobro2004@gmail.com>
2013-11-23 05:00:34 -0600

    added database probe

--- Utility Scripts/DM TEST Asahra/Database Probe.js
+++ Utility Scripts/DM TEST Asahra/Database Probe.js
@@ -17,3 +17,28 @@ function printAssets(globalStateData) {
   error('The assets in the database are: ' + stringify(globalStateData, {newline:false,indent:false }) + ' ' + globalStateData.greeting);
 
 }
+
+handlerCreate({
+  name: 'clickStart',
+  channel: 'direct',
+  message: 'clickStart'
+});
+
+function test(joe) {
+
+  var a;
+
+  var joke={v1:{av:'b'},v2:{cv:'d'},
+  v3:{tess:"la"}};
+
+  var joke2;
+
+  joke2 = {v1:{av:'b'},v2:{cv:'d'},
+    v3:{tess:"la"}};
+  var b = {
+    test: 'jon',
+    sue: 'ellen',
+    taby: 'cat'
+  };
+}
+
