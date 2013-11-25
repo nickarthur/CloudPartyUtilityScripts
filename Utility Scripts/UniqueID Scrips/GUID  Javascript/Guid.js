a0e362af0838a114f951c3e1f3ff189de35740f9
nickarthur <metrobro2004@gmail.com>
2013-11-24 22:59:53 -0600

    added guid unique id code

---
+++ Utility Scripts/UniqueID Scrips/GUID  Javascript/Guid.js
@@ -0,0 +1,15 @@
+﻿function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+document.getElementById('generateUUID').onclick = function() {
+    document.getElementById('uuid').innerHTML = generateUUID();
+};
+
+document.getElementById('uuid').innerHTML = generateUUID();
---
+++ Utility Scripts/UniqueID Scrips/Prefab Unique Name/UniquePrefabName.js
@@ -0,0 +1,1 @@
+﻿var g_myName = getParam('Game Object Name')===''?'prefab'+randomInt(0,70000):getParam('Game Object Name');
