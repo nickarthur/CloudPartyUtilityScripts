0406c4b3ab19f27b23c5c9dd53ebeb76cbefc361
nickarthur <metrobro2004@gmail.com>
2013-11-24 21:35:19 -0600

    Revert "give assets a default name if user doesn't remember to name it"
    This reverts commit 6759d8fe7c8faa4003f3cebb53d837e658389b2a.

--- Utility Scripts/DM TEST Asahra/DM Asset 3.js
+++ Utility Scripts/DM TEST Asahra/DM Asset 3.js
@@ -1,7 +1,7 @@
 ﻿
  // Register...
 
-var g_myName = (getParam('Game Object Name') === '') ?     'prefab' + randomInt(1,60000) : getParam('Game Object Name');
+var g_myName = getParam('Game Object Name');
 
 //http://wiki.cloudparty.com/wiki/Script_Examples/Scene_Scripts
  // Where are we?
@@ -26,8 +26,7 @@ function keepAlive() {
        keys: ['gameObjects'],
        callback: 'reRegister'
      });
-}
-
+}
 function reRegister(globalStateData) {
 
   var keysArray = keys(globalStateData.gameObjects);
